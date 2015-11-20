76a75e6d86b5858bf106de26b18ff55f9463732e
anushinterlocking <anushinterlocking@users.noreply.github.com>
2015-11-19 23:20:02 -0500

    User input validation
    Added input validation for the test. Input must be an integer and within
    a valid range.

--- mentaladd.py
+++ mentaladd.py
@@ -2,6 +2,36 @@ from datetime import datetime
 import random
 import mysql.connector
 
+while True:
+    no_of_questions = raw_input("No. of questions: ")
+    if no_of_questions.isdigit() and (int(no_of_questions) >= 1 and int(no_of_questions) <= 1000):
+        no_of_questions = int(no_of_questions)
+        break
+    else:
+        print "Please enter a valid number between 1 and 1000"
+
+while True:
+    l = raw_input("Range Low: ")
+    if l.isdigit() and (int(l) >= 1 and int(l) <= 1000):
+        l = int(l)
+        break
+    else:
+        print "Please enter a valid number between 1 and 1000"
+
+while True:
+    h = raw_input("Range High: ")
+    if h.isdigit() and (int(h) >= 1 and int(h) <= 1000):
+        h = int(h)
+        break
+    else:
+        print "Please enter a valid number between 1 and 1000"
+
+conn = mysql.connector.connect(user='root', password='Dont4get', host='localhost',database='sakila')
+cur = conn.cursor()
+cur.execute("insert into test(dated, description, low, high, no_of_questions) values ('%s','%s',%s,%s,%s)" % (datetime.now(),'Addition', l,h,no_of_questions))
+test_id = cur.lastrowid
+test = []
+
 class Question:
 	def __init__(self,x,y,ans,time):
 		self.x = x
@@ -9,16 +39,6 @@ class Question:
 		self.ans = ans
 		self.result = 1 if x+y == ans else 0
 		self.time = time
-
-no_of_questions = int(raw_input("No. of questions: "))
-l = int(raw_input("Range Low: "))
-h = int(raw_input("Range High: "))
-
-conn = mysql.connector.connect(user='root', password='Dont4get', host='localhost',database='sakila')
-cur = conn.cursor()
-cur.execute("insert into test(dated, description, low, high, no_of_questions) values ('%s','%s',%s,%s,%s)" % (datetime.now(),'Addition', l,h,no_of_questions))
-test_id = cur.lastrowid
-test = []
 
 for n in range(0,no_of_questions):
 	x = random.randrange(l,h)
